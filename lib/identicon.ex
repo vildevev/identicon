16eb619c4405a0350aa0390bb795ff071ea82a47
“Vilde <vilde@vevatne.no>
2017-07-23 10:59:12 -0700

    Add main and hash_input functions to Identicon module

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,14 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  
+  def main(input) do
+    # First object given to pipe operator will be passed down as first argument.
+    input 
+    |> hash_input
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def hash_input(input) do
+    # Using pipe operator makes more idiomatic Elixir code. 
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
