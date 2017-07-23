a91aa93b9057a8c9adfdc9378a646c5cae3e974d
“Vilde <vilde@vevatne.no>
2017-07-23 14:58:09 -0700

    Add build_pixel_map function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,23 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    # Underscore in front means that we adknowledge that an element is there, but that we don't care about it.
+    # Not an actual index. Just referring to the 'fake' indeces made earlier.
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end 
+    # Must add 'pixel_map' property to struct for this to work.
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
--- lib/image.ex
+++ lib/image.ex
@@ -2,5 +2,5 @@ defmodule Identicon.Image do
 	# Struct is a data structure that stores data. Can be assigned default values.
 	# The only properties that can exist in the struct are the ones we assign in the module file.
 	# With functional programming there are no class method etc.
-	defstruct hex: nil, color: nil, grid: nil
+	defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
